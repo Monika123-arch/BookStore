a29e01505f1636461938f22c6c9372975acd7844
Monika <monikadkale98@gmail.com>
2021-05-20 23:58:30 +0530

    added adminlogin component routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminloginComponent } from './component/adminlogin/adminlogin.component';
 import { LoginComponent } from './component/login/login.component';
 
 const routes: Routes = [
@@ -11,6 +12,10 @@ const routes: Routes = [
   { 
     path: 'login',
     component: LoginComponent
+  },
+  { 
+    path: 'adminlogin',
+    component: AdminloginComponent
   },
 ]
 
