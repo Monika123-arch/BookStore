e6ea9fb72aee0ba263de2afafdb98b41dad39e05
Monika <monikadkale98@gmail.com>
2021-05-24 22:34:42 +0530

    added login validation and api

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder,FormGroup,Validators} from '@angular/forms'
+import { Router } from '@angular/router';
+import{ AdminService } from '../../service/admin/admin.service'
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
- 
-  constructor() { }
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder, private adminService: AdminService,private route:Router)  {
+    this.form = this.formBuilder.group({
+      Email : ['', [Validators.required, Validators.email]],
+      password : ['',[Validators.required]]
+    });
+  }
 
   ngOnInit(): void {
+  }
+  submit(){
+
+    if (this.form.valid) {
+
+      console.log(this.form.value);
+      let reqObj = {
+        email : this.form.value.Email,
+        password : this.form.value.password
+      }
+      let array=[] as any;
+      this.adminService.login(reqObj).subscribe((res)=>{
+        console.log(res)
+        array=res;
+        localStorage.setItem('access',array.result.accessToken)
+
+        this.route.navigate(['/dashboard'])
+      },(error)=>{
+        console.log(error)
+      })
+    }
+
   }
 }
 
