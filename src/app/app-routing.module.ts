b44e5d71e87721959989a5ec3ea4044924cac65d
Monika <monikadkale98@gmail.com>
2021-05-28 21:05:11 +0530

    importing maticon module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { DashboarduserComponent } from './component/dashboarduser/dashboarduser.component';
 import { LoginComponent } from './component/login/login.component';
+import { LoginuserComponent } from './component/loginuser/loginuser.component';
+import { SuccessPageComponent } from './component/success-page/success-page.component';
+import { UserorderComponent } from './component/userorder/userorder.component';
 
 const routes: Routes = [
   {
@@ -17,10 +21,24 @@ const routes: Routes = [
   path: 'dashboard',
   component: DashboardComponent
 },
+{ 
+  path: 'dashboarduser',
+  component: DashboarduserComponent
+},
+{ 
+  path: 'success-page',
+  component: SuccessPageComponent
+},
+{ 
+  path: 'loginuser',
+  component: LoginuserComponent
+},
+{ 
+  path: 'userorder',
+  component: UserorderComponent
+},
+
 ]
-
-
-
 
 
 @NgModule({
