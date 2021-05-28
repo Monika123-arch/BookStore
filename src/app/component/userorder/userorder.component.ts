927f8138e436c8395b57ef516ac79415dde30b2b
Monika <monikadkale98@gmail.com>
2021-05-28 21:26:26 +0530

    adding user order api here

--- src/app/component/userorder/userorder.component.ts
+++ src/app/component/userorder/userorder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/service/user/user.service';
 
 @Component({
   selector: 'app-userorder',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userorder.component.css']
 })
 export class UserorderComponent implements OnInit {
-
-  constructor() { }
-
+  show = 1;
+  show1 = 0;
+  show2 = 1;
+  
+  constructor(private userservice:UserService) { }
+  cartarray = [] as any;
+  display(){
+    this.show += 1;
+  }
+  
+  displaynext(){
+    this.show1 += this.show;
+    this.show2 -= 1;
+  }
   ngOnInit(): void {
+   this.showcart();
   }
 
+
+showcart(){
+  let cart = [] as any;
+  this.userservice.cartshow().subscribe((response)=>{
+    console.log(response);
+    cart = response;
+    this.cartarray=cart.result;
+    console.log(this.cartarray);
+  },(error)=>{
+    console.log(error);
+  })
 }
+}
+
